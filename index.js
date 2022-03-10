196ddfba3aba28f6615b0c0937f4489af40143b7
Nikhil Shakya <shakyanikhil12345@gmail.com>
2022-03-10 07:29:49 +0000

    fetched data

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 console.log(API_URL);
 
-function renderDynamicContent() {
-    document.getElementById('dynamic-content').innerHTML = "Dynamic content"
+fetchData();
+
+async function fetchData  (){
+
+    let res = await fetch("https://codejudge-question-artifacts.s3.ap-south-1.amazonaws.com/poplution-countries-yearwise.json");
+    let data = await res.json();
+    console.log(data);
 }
 
